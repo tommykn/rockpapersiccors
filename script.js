1ad138f0bea504fbaf2086f4709ecfd662dc2bec
Tommy <tbkknight1@icloud.com>
2021-12-18 08:46:31 -0600

    Finished the playRound fucntion that returns a string saying who won, and will play until there is a winner

--- script.js
+++ script.js
@@ -28,14 +28,47 @@ function getPlayerSelection() {
     if (x != 'rock' && x != 'paper' && x != 'scissors') {
         x = prompt("You made a typo please enter in Rock, Paper, or Scissors").toLowerCase();
     }
-    
+
     if (x === 'rock') {
-        playerSelection = 'rock';
+        return 'rock';
     } else if (x === 'paper') {
-        playerSelection = 'paper';
+        return 'paper';
     } else if (x === 'scissors') {
-        playerSelection = 'scissors';
+        return 'scissors';
     } else {
-        getPlayerSelection();
+        return getPlayerSelection();
     }
 }
+let computerSelection = computerPlay();
+
+/* we create the playround function with takes in playerselction and
+computer selection then sets playerselction equal to the getPlayerSelection function
+and computerSElection equeal to computerPlay. Next it will comapare all possible
+outcome and return the conssidonding string that says who won with if statement. If 
+there is a tie it will retuen playRound so they will play agian.*/
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = getPlayerSelection();
+    computerSelection = computerPlay();
+    console.log("You choose:", playerSelection);
+    console.log("Computer choose:", computerSelection);
+
+    if (computerSelection === 'rock' && playerSelection === 'scissors') {
+        return "Computer Wins! Rock beats scissors.";
+    } else if (computerSelection === 'paper' && playerSelection === 'rock') {
+        return "Computer Wins! Paper beats rock.";
+    } else if (computerSelection === 'scissors' && playerSelection === 'paper') {
+        return "Computer Wins! Scissors beats paper.";
+    } else if (computerSelection === 'scissors' && playerSelection === 'rock') {
+        return "You Win! Rock beats scissors.";
+    } else if (computerSelection === 'rock' && playerSelection === 'paper') {
+        return "You Win! Paper beats rock.";
+    } else if (computerSelection === 'paper' && playerSelection === 'scissors') {
+        return "You Win! Scissors beats paper.";
+    } else {
+        alert("It's a tie! Click ok to play again!");
+        return playRound(playerSelection, computerSelection);
+    }
+}
+
+
