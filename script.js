2f9f9f006e04d02e3e273ef5577c06135da24d2f
Tommy <tbkknight1@icloud.com>
2021-12-18 14:11:11 -0600

    Finished the game function and the program is now completetly functional for a game of five rounds

--- script.js
+++ script.js
@@ -47,6 +47,11 @@ and computerSElection equeal to computerPlay. Next it will comapare all possible
 outcome and return the conssidonding string that says who won with if statement. If 
 there is a tie it will retuen playRound so they will play agian.*/
 
+/* Create a var that keeps trace of wins for player and one for computer.*/
+
+let playerWins = 0;
+let computerWins = 0;
+
 function playRound(playerSelection, computerSelection) {
     playerSelection = getPlayerSelection();
     computerSelection = computerPlay();
@@ -54,20 +59,53 @@ function playRound(playerSelection, computerSelection) {
     console.log("Computer choose:", computerSelection);
 
     if (computerSelection === 'rock' && playerSelection === 'scissors') {
+        computerWins++;
+        console.log("Computer Wins! Rock beats scissors.");
         return "Computer Wins! Rock beats scissors.";
     } else if (computerSelection === 'paper' && playerSelection === 'rock') {
+        computerWins++;
+        console.log("Computer Wins! Paper beats rock.");
         return "Computer Wins! Paper beats rock.";
     } else if (computerSelection === 'scissors' && playerSelection === 'paper') {
+        computerWins++;
+        console.log("Computer Wins! Scissors beats paper.");
         return "Computer Wins! Scissors beats paper.";
     } else if (computerSelection === 'scissors' && playerSelection === 'rock') {
+        playerWins++;
+        console.log("You Win! Rock beats scissors.");
         return "You Win! Rock beats scissors.";
     } else if (computerSelection === 'rock' && playerSelection === 'paper') {
+        playerWins++;
+        console.log("You Win! Paper beats rock.");
         return "You Win! Paper beats rock.";
     } else if (computerSelection === 'paper' && playerSelection === 'scissors') {
+        playerWins++;
+        console.log("You win! Scissors beats paper.");
         return "You Win! Scissors beats paper.";
     } else {
         alert("It's a tie! Click ok to play again!");
         return playRound(playerSelection, computerSelection);
+    }
+}
+
+
+/* create a game function that uses a for loop to run the playRound function 
+five times then if playerWins >= 3 alert you win the best of five else alert 
+computer wins the best of five*/
+
+function game() {
+    computerWins = 0;
+    playerWins = 0;
+    for (let i = 0; i < 5; i++) {
+        playRound(playerSelection, computerSelection);
+    }
+    console.log(playerWins);
+    console.log(computerWins);
+
+    if (playerWins >= 3) {
+        alert(`Congratulations you won the best of five! The score was computer: ${computerWins} you: ${playerWins}`);
+    } else {
+        alert(`Sorry the computer won the best of five. The score was computer: ${computerWins} you: ${playerWins}`);
     }
 }
 
